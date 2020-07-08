13da61bd40acf8331aba4b1b63878b4cc265e34b
Daniel Vega <danielalexvega@gmail.com>
2020-07-08 16:49:14 -0500

    added questions array

---
+++ index.js
@@ -0,0 +1,24 @@
+// array of questions for user
+const questions = [
+    'What is your project title?',
+    'Write a description of your project:',
+    'What are the installation instructions?',
+    'What is the usage information?',
+    'What are the contribution guidelines?',
+    'What are the test instructions?',
+    'What type of licence do you want?',
+    'What is your GitHub username?',
+    'What is your email address?'
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
